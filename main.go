3a0c3df3977fdd21850009a62c479e143caa602a
Asep Saepudin <asepsaepudin@msn.com>
2017-07-16 03:11:51 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+      <html>
+        <head>
+          <title>Hello, World!</title>
+        </head>
+        <body>
+          <h1>Hello, World!</h1>
+        </body>
+      </html>
+    `))
+	}
+
+	http.HandleFunc("/", handler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
